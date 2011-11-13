bc6e817864a7a0577f103f7d5607832ad8a2465b
Hanhong Xue <hanhong@us.ibm.com>
2011-11-13 00:10:45 -0500

    Change Nonogram input format to have rows before columns

--- Nonogram.cpp
+++ Nonogram.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class Nonogram : public Problem<bool>
 {
     public:
-        Nonogram(Option option);
+        Nonogram(Option option, bool rootate);
         void ShowState(Variable<bool> *);
         void ShowSolution();
 
@@ -20,28 +20,38 @@ class Nonogram : public Problem<bool>
         vector< vector<int> >  row_runs;
 };
 
-Nonogram::Nonogram(Option option)
+Nonogram::Nonogram(Option option, bool rotate)
     : Problem<bool>(option)
 {
     // Read input
     char line[80];
     fgets(line, sizeof(line), stdin);
-    sscanf(line, "%d %d", &columns, &rows);
+    if (!rotate)
+        sscanf(line, "%d %d", &rows, &columns);
+    else
+        sscanf(line, "%d %d", &columns, &rows);
 
-    column_runs.resize(columns);
     row_runs.resize(rows);
-    for (int i = 0; i < columns + rows; i++) {
-        fgets(line, sizeof(line), stdin);
+    column_runs.resize(columns);
+    for (int i = 0; i < rows + columns; i++) {
+        do fgets(line, sizeof(line), stdin);
+        while (line[0] == '#');
         int n[16];
         int count = sscanf(line, "%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", 
                 n+0, n+1, n+2, n+3, n+4, n+5, n+6, n+7, 
                 n+8, n+9, n+10, n+11, n+12, n+13, n+14, n+15);
 
         for (int j = 0; j < count; j++) {
-            if (i < columns)
-                column_runs[i].push_back(n[j]);
+            if (!rotate)
+                if (i < rows)
+                    row_runs[i].push_back(n[j]);
+                else
+                    column_runs[i-rows].push_back(n[j]);
             else
-                row_runs[i-columns].push_back(n[j]);
+                if (i < columns)
+                    column_runs[i].push_back(n[j]);
+                else
+                    row_runs[i-columns].push_back(n[j]);
         }
     }
 
@@ -52,18 +62,18 @@ Nonogram::Nonogram(Option option)
         for (int y = 0; y < rows; y++)
             grid[x].push_back( new Variable<bool>(domain, 2) );
 
-    // Create column constraints
-    for (int x = 0; x < columns; x++) {
-        RunLength *r = new RunLength(column_runs[x]);
-        for (int y = 0; y < rows; y++)
-            r->AddVariable(grid[x][y]);
-        AddConstraint(r);
-    }
-
     // Create row constraints
     for (int y = 0; y < rows; y++) {
         RunLength  *r = new RunLength(row_runs[y]);
         for (int x = 0; x < columns; x++) 
+            r->AddVariable(grid[x][y]);
+        AddConstraint(r);
+    }
+
+    // Create column constraints
+    for (int x = 0; x < columns; x++) {
+        RunLength *r = new RunLength(column_runs[x]);
+        for (int y = 0; y < rows; y++)
             r->AddVariable(grid[x][y]);
         AddConstraint(r);
     }
@@ -98,11 +108,15 @@ void Nonogram::ShowSolution()
 
 int main(int argc, char *argv[])
 {
+    bool rotate = false;
+
     Option option;
     option.sort = Option::SORT_FAILURES;
     option.GetOptions(argc, argv);
+    if (optind < argc)
+        rotate = atoi(argv[optind]) != 0;
 
-    Nonogram puzzle(option);
+    Nonogram puzzle(option, rotate);
     puzzle.Solve();
 
     return 0;
