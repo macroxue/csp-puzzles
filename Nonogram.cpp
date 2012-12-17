f9c502b2cde3b5bd078ad6dab7e5b2e59fc2bc96
Hanhong Xue <hanhong@us.ibm.com>
2012-12-16 21:52:45 -0500

    Add a few new Nonogram puzzles

--- Nonogram.cpp
+++ Nonogram.cpp
@@ -36,10 +36,14 @@ Nonogram::Nonogram(Option option, bool rotate)
     for (int i = 0; i < rows + columns; i++) {
         do fgets(line, sizeof(line), stdin);
         while (line[0] == '#');
-        int n[16];
-        int count = sscanf(line, "%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", 
+        int n[32];
+        int count = sscanf(line,
+                "%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d"
+                "%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", 
                 n+0, n+1, n+2, n+3, n+4, n+5, n+6, n+7, 
-                n+8, n+9, n+10, n+11, n+12, n+13, n+14, n+15);
+                n+8, n+9, n+10, n+11, n+12, n+13, n+14, n+15,
+                n+16, n+17, n+18, n+19, n+20, n+21, n+22, n+23,
+                n+24, n+25, n+26, n+27, n+28, n+29, n+30, n+31);
 
         for (int j = 0; j < count; j++) {
             if (!rotate)
