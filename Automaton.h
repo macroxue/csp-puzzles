5432cff51e044c8d9af2bcd9af224072b3db224e
Hanhong Xue <hanhong@us.ibm.com>
2013-01-01 21:44:34 -0500

    Reduce memory for recording transitions

--- Automaton.h
+++ Automaton.h
@@ -40,23 +40,21 @@ class Automaton
 
     private:
 
-        struct State {
-            size_t           id;
-            size_t           dist_to_acc; // distance to accepting state
-            State *          transition[N];
-
-            State(size_t id = 0);
-            State *Transit(T value);
-        }; 
+        struct State;
 
         struct Transition {
             State   *from;
             State   *to;
             T       value;
-
-            Transition(State *from, State *to, T value)
-                : from(from), to(to), value(value) {}
         };
+
+        struct State {
+            size_t           id;
+            size_t           dist_to_acc; // distance to accepting state
+            Transition       transitions[N];
+
+            State(size_t id = 0);
+        }; 
 
         struct PowerState {
             Set<M>                 member_states;
@@ -65,11 +63,10 @@ class Automaton
             Set<N>                 member_inputs;
             size_t                 num_inputs;
             T                      inputs[N];
-            vector<Transition>     transition;
+            vector<Transition *>   transitions;
 
             void AddState(State *state);
             void AddInput(T value);
-            void AddTransition(State *from, State *to, T value);
         };
 
         State             * start;
@@ -90,11 +87,11 @@ Automaton<T,N,M>::Automaton(vector<Run> &run)
         for (size_t n = 0; n < run[i].count; n++) {
             State *new_state = new State(all_states.size());
             all_states.push_back(new_state);
-            accept->transition[value] = new_state;
+            accept->transitions[value].to = new_state;
             accept = new_state;
         }
         if (run[i].mod == Run::AT_LEAST)
-            accept->transition[value] = accept;
+            accept->transitions[value].to = accept;
     }
 
     // caculate distance to accepting state
@@ -117,7 +114,7 @@ bool Automaton<T,N,M>::Accept(Input<I> &input, size_t input_size)
         power_states[i].num_states = 0;
         power_states[i].member_inputs.Clear();
         power_states[i].num_inputs = 0;
-        power_states[i].transition.clear();
+        power_states[i].transitions.clear();
     }
 
     power_states[0].AddState(start);
@@ -128,23 +125,25 @@ bool Automaton<T,N,M>::Accept(Input<I> &input, size_t input_size)
         PowerState &next_power_state = power_states[i+1];
 
         for (size_t s = 0; s < power_state.num_states; s++) {
+            State *state = power_state.states[s];
             for (size_t v = 0; v < N; v++) {
-                T  value = (T)v;
-                if (input.IsDecided(i) && value != input.GetValue(i))
+                Transition &transition = state->transitions[v];
+
+                // skip invalid transition
+                if (transition.to == NULL)
                     continue;
 
-                State *state = power_state.states[s];
-                State *next_state = state->Transit(value);
-                if (next_state == NULL)
+                // skip mismatched input
+                if (input.IsDecided(i) && transition.value != input.GetValue(i))
                     continue;
 
                 // reject state that's too far from accepting
                 size_t num_inputs_left = input_size - 1 - i;
-                if (next_state->dist_to_acc > num_inputs_left) 
+                if (transition.to->dist_to_acc > num_inputs_left) 
                     continue;
 
-                power_state.AddTransition(state, next_state, value);
-                next_power_state.AddState(next_state);
+                power_state.transitions.push_back(&transition);
+                next_power_state.AddState(transition.to);
             }
         }
 
@@ -159,8 +158,8 @@ bool Automaton<T,N,M>::Accept(Input<I> &input, size_t input_size)
 
         // gather valid states and inputs
         power_state.member_states.Clear();
-        for (size_t t = 0; t < power_state.transition.size(); t++) {
-            Transition &transition = power_state.transition[t];
+        for (size_t t = 0; t < power_state.transitions.size(); t++) {
+            Transition &transition = *power_state.transitions[t];
             if (next_power_state.member_states.Has(transition.to->id)) {
                 power_state.member_states.Add(transition.from->id);
                 power_state.AddInput(transition.value);
@@ -180,14 +179,11 @@ template <class T, size_t N, size_t M>
 Automaton<T,N,M>::State::State(size_t id)
     : id(id)
 {
-    for (size_t i = 0; i < N; i++)
-        transition[i] = NULL;
-}
-
-template <class T, size_t N, size_t M>
-typename Automaton<T,N,M>::State *Automaton<T,N,M>::State::Transit(T value)
-{
-    return transition[value];
+    for (size_t v = 0; v < N; v++) {
+        transitions[v].from  = this;
+        transitions[v].to    = NULL;
+        transitions[v].value = (T)v;
+    }
 }
 
 template <class T, size_t N, size_t M>
@@ -206,12 +202,6 @@ void Automaton<T,N,M>::PowerState::AddInput(T value)
         member_inputs.Add(value);
         inputs[num_inputs++] = value;
     }
-}
-
-template <class T, size_t N, size_t M>
-void Automaton<T,N,M>::PowerState::AddTransition(State *from, State *to, T value)
-{
-    transition.push_back(Transition(from, to, value));
 }
 
 #endif
